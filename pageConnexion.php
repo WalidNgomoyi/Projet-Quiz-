350266bd2934d5ee68fc54c763ed1702e333a4b7
WalidNgomoyi <walid-dickaella@ism.edu.sn>
2020-06-02 15:55:55 +0000

    projet

---
+++ pageConnexion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>GESCO</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/bootstrap4/css/bootstrap.css">
+    <link rel="stylesheet" href="style1.css">
+
+</head>
+
+<body>
+    <div class="mainContainer">
+        <div class="headerContainer">
+            <div>
+                <img src="Images/Icônes/logo.PNG" alt="image" class="img1">
+                <h2 class="h2">Le plaisir de jouer </h2>
+            </div>
+
+        </div>
+        <div class="bodyContainer">
+
+            <div class="card form1">
+                <div class="card-header ">
+                    Login Form
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+
+
+
+
+                            <input type="text" class="form-control " placeholder="Login">
+                            <span class="glyphicon glyphicon-user"></span>
+
+
+                        </div>
+                        <div class="form-group">
+
+                            <input type="text" class="form-control" placeholder="Password">
+                        </div>
+
+                        <button type="submit" class="button">Connexion</button>
+                        <p class="paraph1">S'incrire pour jouer?</p>
+                    </form>
+                </div>
+
+
+
+
+
+            </div>
+        </div>
+        <script src="assets/jquery/jquery.js"></script>
+        <script src="assets/bootstrap4/js/bootstrap.js"></script>
+</body>
+
+</html>
